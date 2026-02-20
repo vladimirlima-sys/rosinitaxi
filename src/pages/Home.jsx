8a33039cb2ce7823215bfb814754cdc025326bb1
Rosini Táxi <taxirosini@gmail.com>
2026-02-20 10:28:56 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import FooterSection from '@/components/home/FooterSection';
 import LanguageSwitcher from '@/components/home/LanguageSwitcher';
 import ChatSupport from '@/components/home/ChatSupport';
 import BookingsModal from '@/components/bookings/BookingsModal';
+import ReviewList from '@/components/reviews/ReviewList';
 import { LanguageProvider } from '@/components/LanguageContext';
 
 export default function Home() {
