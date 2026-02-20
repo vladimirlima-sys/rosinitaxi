5acb4f2f005dda6a82a32ec141b889738ee3ffb2
Rosini Táxi <taxirosini@gmail.com>
2026-02-20 11:50:25 +0000

    File changes

--- src/components/home/BookingForm.jsx
+++ src/components/home/BookingForm.jsx
@@ -361,16 +361,7 @@ export default function BookingForm({ bookingRef }) {
               </div>
             </div>
 
-            {form.departure_point && form.arrival_point && (
-              <div className="space-y-4">
-                <RouteMap 
-                  departure={form.departure_point} 
-                  arrival={form.arrival_point} 
-                  distance_km={estimatedDistance}
-                  onRouteCalculated={handleRouteCalculated}
-                />
-              </div>
-            )}
+
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div className="space-y-2">
