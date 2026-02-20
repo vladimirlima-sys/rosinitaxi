3bacbd1041650cd0d1a5d7a8c89bc5264b4d1950
Rosini Táxi <taxirosini@gmail.com>
2026-02-20 10:57:31 +0000

    File changes

--- src/components/home/BookingForm.jsx
+++ src/components/home/BookingForm.jsx
@@ -6,6 +6,8 @@ import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import VehicleCard from './VehicleCard';
 import RouteMap from './RouteMap';
+import RoutePreferences from './RoutePreferences';
+import WaypointsList from './WaypointsList';
 import PricingBreakdown from './PricingBreakdown';
 import { base44 } from '@/api/base44Client';
 import { toast } from 'sonner';
