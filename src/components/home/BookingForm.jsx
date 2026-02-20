bad83e9eccb53e9197f425cab1d419390a6739ba
Rosini Táxi <taxirosini@gmail.com>
2026-02-20 17:42:24 +0000

    File changes

--- src/components/home/BookingForm.jsx
+++ src/components/home/BookingForm.jsx
@@ -262,6 +262,7 @@ export default function BookingForm({ bookingRef }) {
 
   const resetForm = () => {
     setStep(1);
+    setPaymentMethod('stripe');
     setForm({ departure_point: '', arrival_point: '', departure_date: '', departure_time: '', flight_number: '', vehicle_type: 'economic', passengers: 1, client_name: '', client_email: '', client_phone: '', notes: '', driver_preference: '', distance_km: 0 });
     setEstimatedDistance(0);
     setEstimatedTime(0);
