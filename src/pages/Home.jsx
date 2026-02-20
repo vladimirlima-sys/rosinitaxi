52c8dcbc27cd4c90e7e8f65c1f8e46fb37e2c9b4
Rosini Táxi <taxirosini@gmail.com>
2026-02-20 02:51:57 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,6 +22,7 @@ export default function Home() {
         <ServicesSection />
         <BookingForm bookingRef={bookingRef} />
         <FooterSection />
+        <ChatSupport />
       </div>
     </LanguageProvider>
   );
