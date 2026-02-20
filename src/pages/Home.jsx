f74c577bfbb024c8a24de2aaa97e9f9779bdbb8c
Rosini Táxi <taxirosini@gmail.com>
2026-02-20 02:51:39 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import ServicesSection from '@/components/home/ServicesSection';
 import BookingForm from '@/components/home/BookingForm';
 import FooterSection from '@/components/home/FooterSection';
 import LanguageSwitcher from '@/components/home/LanguageSwitcher';
+import ChatSupport from '@/components/home/ChatSupport';
 import { LanguageProvider } from '@/components/LanguageContext';
 
 export default function Home() {
