150b3e0c463e532255b5935cbcbff0803e15f7bf
Rosini Táxi <taxirosini@gmail.com>
2026-02-20 17:22:51 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Bookmark } from 'lucide-react';
 import HeroSection from '@/components/home/HeroSection';
 import ServicesSection from '@/components/home/ServicesSection';
 import BookingForm from '@/components/home/BookingForm';
-import ReviewsSection from '@/components/home/ReviewsSection';
+import ReviewsSection from '@/components/home/ReviewsSection.jsx';
 import FooterSection from '@/components/home/FooterSection';
 import LanguageSwitcher from '@/components/home/LanguageSwitcher';
 import ChatSupport from '@/components/home/ChatSupport';
