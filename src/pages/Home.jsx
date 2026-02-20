0a0bad5f861aa66422930039504a84d5fda2a798
Rosini Táxi <taxirosini@gmail.com>
2026-02-20 14:13:24 +0000

    Badge Raio Animado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,9 @@ import ServicesSection from '@/components/home/ServicesSection';
 import BookingForm from '@/components/home/BookingForm';
 import FooterSection from '@/components/home/FooterSection';
 import LanguageSwitcher from '@/components/home/LanguageSwitcher';
+import ChatSupport from '@/components/home/ChatSupport';
 import BookingsModal from '@/components/bookings/BookingsModal';
-import FloatingActions from '@/components/home/FloatingActions';
+import LastMinuteTransferBadge from '@/components/home/LastMinuteTransferBadge';
 import { LanguageProvider } from '@/components/LanguageContext';
 
 export default function Home() {
@@ -20,13 +21,23 @@ export default function Home() {
   return (
     <LanguageProvider>
       <div className="min-h-screen bg-[#0A0A0A]">
+        <LastMinuteTransferBadge />
         <LanguageSwitcher />
         <HeroSection onScrollToBooking={scrollToBooking} />
         <ServicesSection />
         <BookingForm bookingRef={bookingRef} />
         <FooterSection />
-        
-        <FloatingActions onOpenBookingsModal={() => setShowBookingsModal(true)} />
+        <ChatSupport />
+
+        {/* Floating button for bookings */}
+        <button
+          onClick={() => setShowBookingsModal(true)}
+          className="fixed bottom-6 left-6 z-40 w-14 h-14 rounded-full bg-[#C9A96E] hover:bg-[#B8955D] text-[#0A0A0A] shadow-lg flex items-center justify-center transition-all hover:scale-110"
+          title="Mes réservations"
+        >
+          <Bookmark className="w-6 h-6" />
+        </button>
+
         <BookingsModal isOpen={showBookingsModal} onClose={() => setShowBookingsModal(false)} />
       </div>
     </LanguageProvider>
