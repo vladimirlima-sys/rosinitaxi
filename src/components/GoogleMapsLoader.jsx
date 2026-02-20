2f01ea7f270d4cc124ccb518d6c17cf037cf0a2b
Rosini Táxi <taxirosini@gmail.com>
2026-02-20 19:59:53 +0000

    File changes

--- src/components/GoogleMapsLoader.jsx
+++ src/components/GoogleMapsLoader.jsx
@@ -5,7 +5,7 @@ export default function GoogleMapsLoader() {
     if (window.google) return;
 
     const script = document.createElement('script');
-    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAdmO8dotir_WQEV-27tmhZjUh_6T0o010&libraries=places,directions';
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAdmO8dotir_WQEV-27tmhZjUh_6T0o010&libraries=places,routes';
     script.async = true;
     script.defer = true;
     document.head.appendChild(script);
