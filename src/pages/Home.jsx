0a08f33bd30ab044b9003addcb1290958956b292
Rosini Táxi <taxirosini@gmail.com>
2026-02-21 01:08:17 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <LanguageProvider>
       <GoogleMapsLoader />
-      <div className="min-h-screen bg-[#0A0A0A]">
+      <div className="min-h-screen bg-black">
         <LanguageSwitcher />
         <HeroSection onScrollToBooking={scrollToBooking} />
         <ServicesSection />
