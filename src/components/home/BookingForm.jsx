806585dfb6944ea27aa531b1cc71a8d8545881c6
Rosini Táxi <taxirosini@gmail.com>
2026-02-22 15:45:12 +0000

    File changes

--- src/components/home/BookingForm.jsx
+++ src/components/home/BookingForm.jsx
@@ -304,7 +304,6 @@ export default function BookingForm({ bookingRef }) {
     setEstimatedTime(0);
     setDynamicPrice(null);
     setCurrentRoute(null);
-    setSelectedExtras([]);
   };
 
   return (
