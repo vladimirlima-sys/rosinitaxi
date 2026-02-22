738512ac934da1b30e4f2ac8597d3fd135002794
Rosini Táxi <taxirosini@gmail.com>
2026-02-22 15:41:30 +0000

    File changes

--- src/components/home/BookingForm.jsx
+++ src/components/home/BookingForm.jsx
@@ -11,8 +11,7 @@ import PlacesAutocomplete from './PlacesAutocomplete';
 import RouteCalculator from './RouteCalculator';
 import RouteMapDisplay from './RouteMapDisplay';
 import RouteCard from './RouteCard';
-      import ExtrasSelector from './ExtrasSelector';
-      import AvailabilityCalendar from './AvailabilityCalendar';
+      
 
       import { base44 } from '@/api/base44Client';
 import { toast } from 'sonner';
