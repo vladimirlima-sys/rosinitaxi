05c016f949db80fad3414b6a8ee17ee0c7840a9e
Rosini Táxi <taxirosini@gmail.com>
2026-02-24 12:22:53 +0000

    File changes

--- src/components/home/BookingForm.jsx
+++ src/components/home/BookingForm.jsx
@@ -400,7 +400,7 @@ export default function BookingForm({ bookingRef }) {
         {step === 3 && (
           <div className="space-y-3">
             <div className="bg-black border border-black/40 rounded-xl p-4 space-y-4">
-              <h3 className="text-black font-semibold text-sm uppercase tracking-wider">{t.step3Title}</h3>
+              <h3 className="text-white font-semibold text-sm uppercase tracking-wider">{t.step3Title}</h3>
 
               <div>
                 <label className={labelClass}><User className="inline w-3 h-3 mr-1" />{t.nameLabel}</label>
