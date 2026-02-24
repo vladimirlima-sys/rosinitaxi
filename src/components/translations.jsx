3bc2bd8cfa551b22f632f38fc3d0915d0585e1c1
Rosini Táxi <taxirosini@gmail.com>
2026-02-24 22:48:05 +0000

    File changes

--- src/components/translations.jsx
+++ src/components/translations.jsx
@@ -207,7 +207,7 @@ export const translations = {
     availableNow: 'Disponible maintenant',
     vehicleFeatures: {
       economic: ['Berline confortable', 'Climatisation', 'Wi-Fi gratuit', 'Bagages inclus'],
-      comfort: ['Wi-Fi gratuit', 'Eau', '3 bagages inclus'],
+      comfort: ['Véhicules plus spacieux', 'Wi-Fi gratuit', 'Eau', '3 bagages inclus'],
     },
     perKm: '/km',
     journeyDetails: 'Détails du voyage',
