3ca1953ac4308cda3d2c38f5e5286a8934f925a8
Rosini Táxi <taxirosini@gmail.com>
2026-02-24 22:58:55 +0000

    File changes

--- src/components/home/BookingForm.jsx
+++ src/components/home/BookingForm.jsx
@@ -526,7 +526,7 @@ export default function BookingForm({ bookingRef }) {
             <div className="flex gap-3">
               <button
                 onClick={() => setStep(3)}
-                className="flex-1 h-12 rounded-xl border border-white/20 text-white/60 font-semibold text-sm hover:bg-white/10 transition-all"
+                className="flex-1 h-12 rounded-xl border-2 border-black bg-white text-black font-semibold text-sm hover:bg-black/10 transition-all"
               >
                 {t.backBtn}
               </button>
