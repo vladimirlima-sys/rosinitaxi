b6dd6bb87ddcb698207aeb311a02a0db8f313fa3
Rosini Táxi <taxirosini@gmail.com>
2026-02-25 22:36:07 +0000

    File changes

--- functions/sendBookingConfirmation.ts
+++ functions/sendBookingConfirmation.ts
@@ -207,14 +207,25 @@ Deno.serve(async (req) => {
     </body>
     </html>`;
 
-    // Send emails using Base44 SendEmail integration
+    // Send emails using Gmail connector
     const sendEmail = async (to, subject, htmlBody) => {
-      return await base44.integrations.Core.SendEmail({
-        to,
-        subject,
-        body: htmlBody,
-        from_name: 'Rosini Transfert'
+      const accessToken = await base44.asServiceRole.connectors.getAccessToken('gmail');
+      const message = `To: ${to}\r\nSubject: ${subject}\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n${htmlBody}`;
+      const encodedMessage = btoa(message).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+      
+      const response = await fetch('https://www.googleapis.com/gmail/v1/users/me/messages/send', {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ raw: encodedMessage })
       });
+      
+      if (!response.ok) {
+        throw new Error(`Gmail API error: ${response.statusText}`);
+      }
+      return response.json();
     };
 
     // Subject lines per language
