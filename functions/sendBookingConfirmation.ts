5ff53496d7b2313aac6b4ac5f1a093a37a020438
Rosini Táxi <taxirosini@gmail.com>
2026-02-25 23:41:00 +0000

    File changes

--- functions/sendBookingConfirmation.ts
+++ functions/sendBookingConfirmation.ts
@@ -212,11 +212,15 @@ Deno.serve(async (req) => {
     const sendGmailEmail = async (to, subject, htmlBody) => {
       const accessToken = await base44.asServiceRole.connectors.getAccessToken('gmail');
       
-      // Build MIME message with proper UTF-8 encoding
-      const emailMessage = `From: taxirosini@gmail.com\r\nTo: ${to}\r\nSubject: ${subject}\r\nMIME-Version: 1.0\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n${htmlBody}`;
+      // Encode subject header properly
+      const subjectEncoded = Buffer.from(subject, 'utf8').toString('base64');
+      const subjectHeader = `=?UTF-8?B?${subjectEncoded}?=`;
       
-      // Encode to base64url for Gmail raw API
-      const base64Message = encode(emailMessage).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+      // Build MIME message - keep body as plain UTF-8
+      const emailMessage = `From: taxirosini@gmail.com\r\nTo: ${to}\r\nSubject: ${subjectHeader}\r\nMIME-Version: 1.0\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Transfer-Encoding: 8bit\r\n\r\n${htmlBody}`;
+      
+      // Encode entire message to base64url for Gmail API
+      const base64Message = Buffer.from(emailMessage, 'utf8').toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
       
       const response = await fetch('https://www.googleapis.com/gmail/v1/users/me/messages/send', {
         method: 'POST',
