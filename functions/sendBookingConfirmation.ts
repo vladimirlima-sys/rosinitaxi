fc0f24127f4681adc6581d6e7c71c213e22de388
Rosini Táxi <taxirosini@gmail.com>
2026-02-25 23:59:15 +0000

    File changes

--- functions/sendBookingConfirmation.ts
+++ functions/sendBookingConfirmation.ts
@@ -1,5 +1,5 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
-import { encode } from 'npm:js-base64';
+import { jsPDF } from 'npm:jspdf@4.0.0';
 
 Deno.serve(async (req) => {
   try {
