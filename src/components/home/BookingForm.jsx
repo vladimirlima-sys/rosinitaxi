7f89ada56310bb428e089f23b872d7889539c69a
Rosini Táxi <taxirosini@gmail.com>
2026-03-01 16:43:56 +0000

    File changes

--- src/components/home/BookingForm.jsx
+++ src/components/home/BookingForm.jsx
@@ -16,7 +16,6 @@ import { toast } from 'sonner';
 import { useLang } from '@/components/LanguageContext';
 import { translations } from '@/components/translations';
 import { createPageUrl } from '@/utils';
-import { createPageUrl } from '@/utils';
 
 export default function BookingForm({ bookingRef }) {
   const { lang } = useLang();
