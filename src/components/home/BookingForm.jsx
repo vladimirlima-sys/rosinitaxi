4563c57b5cf8ab98952aee79b403e36919d983f5
Rosini Táxi <taxirosini@gmail.com>
2026-03-01 18:18:49 +0000

    File changes

--- src/components/home/BookingForm.jsx
+++ src/components/home/BookingForm.jsx
@@ -184,8 +184,6 @@ export default function BookingForm({ bookingRef }) {
 
   const [isCalculatingRoute, setIsCalculatingRoute] = useState(false);
 
-  const [isCalculatingRoute, setIsCalculatingRoute] = useState(false);
-
   const canProceedStep1 = form.departure_point && form.arrival_point && form.departure_date && form.departure_time && estimatedDistance > 0;
   const canProceedStep2 = !!form.vehicle_type;
   const canProceedStep3 = form.client_name && form.client_email && form.client_phone;
