de3b5796ab7afd16d8693bddaed7082d035834f3
Rosini Táxi <taxirosini@gmail.com>
2026-03-02 13:09:24 +0000

    File changes

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { createPageUrl } from '@/utils';
 import { Lock, LayoutDashboard, Users, CreditCard, Calendar, Settings, Car, Star, FileText, LogOut, BarChart3, MessageCircle } from 'lucide-react';
 
