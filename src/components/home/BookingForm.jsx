3421f734d245cfe63bf516466132866a70f9239d
Rosini Táxi <taxirosini@gmail.com>
2026-03-04 23:06:30 +0000

    File changes

--- src/components/home/BookingForm.jsx
+++ src/components/home/BookingForm.jsx
@@ -445,7 +445,7 @@ export default function BookingForm({ bookingRef }) {
             </div>
 
             {/* Passengers */}
-            <div className="bg-black border border-black/40 rounded-xl p-4">
+             <div className="bg-black border border-black/40 rounded-xl p-3 sm:p-4">
               <label className={labelClass}>{t.passengersLabel}</label>
               <div className="flex gap-2 mt-2">
                 {[1, 2, 3, 4].map((num) => (
