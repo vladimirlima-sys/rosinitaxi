139f5c8256e2711f2639dc741d14acd7581b7946
Rosini Táxi <taxirosini@gmail.com>
2026-03-05 21:11:46 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import BookingForm from '@/components/home/BookingForm';
+import HomeLayout from '@/components/home/HomeLayout';
 import LanguageSwitcher from '@/components/home/LanguageSwitcher';
 import SeoHead from '@/components/SeoHead';
 import { LanguageProvider, useLang } from '@/components/LanguageContext';
@@ -11,17 +11,22 @@ function HomeContent({ bookingRef }) {
   return (
     <>
       <SeoHead lang={lang} />
-      <div className="min-h-screen bg-[#F5C300] relative">
+      <div className="relative w-full h-screen">
+        <HomeLayout bookingRef={bookingRef} />
+        
+        {/* Fixed Header Controls */}
         <a
-          href={createPageUrl('FAQ')} className="fixed top-5 left-5 z-50 bg-black text-[#F5C300] p-2.5 opacity-40 rounded-full flex items-center gap-1.5 hover:bg-black/80 transition-all shadow">
+          href={createPageUrl('FAQ')} 
+          className="fixed top-5 left-5 z-50 bg-black text-[#F5C300] p-2.5 rounded-full flex items-center gap-1.5 hover:bg-black/80 transition-all shadow"
+        >
           <HelpCircle className="w-5 h-5" />
         </a>
+        
         <div className="fixed top-5 right-5 z-50">
           <LanguageSwitcher />
         </div>
 
-        <BookingForm bookingRef={bookingRef} />
-        <div className="absolute bottom-0 right-0 p-4">
+        <div className="fixed bottom-6 right-6 z-40">
           <a href={createPageUrl('DriverPortal')} title="Portal do Motorista">
             <img src="https://flagcdn.com/ch.svg" alt="Suíça" className="w-12 h-8 rounded shadow-lg cursor-pointer hover:opacity-80 transition-opacity" />
           </a>
